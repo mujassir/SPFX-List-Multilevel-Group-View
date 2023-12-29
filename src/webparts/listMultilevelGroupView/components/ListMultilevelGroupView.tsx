084d1462c8cd74ac877b4bb3623f44d00972e3bb
Mujassir Nasir <mujassir@gmail.com>
2023-12-29 11:38:54 +0500

    Get data from sandbox list

--- src/webparts/listMultilevelGroupView/components/ListMultilevelGroupView.tsx
+++ src/webparts/listMultilevelGroupView/components/ListMultilevelGroupView.tsx
@@ -8,6 +8,15 @@ import "@pnp/sp/webs";
 import "@pnp/sp/lists";
 import "@pnp/sp/items";
 
+const fieldNames = {
+
+  name: 'field_1',
+  country: 'field_2',
+  region: 'field_3',
+  website: 'field_4',
+  category: 'field_6'
+
+};
 export default class ListMultilevelGroupView extends React.Component<IListMultilevelGroupViewProps, {}> {
   private _sp: SPFI;
 
@@ -36,8 +45,10 @@ export default class ListMultilevelGroupView extends React.Component<IListMultil
 
   private async getListData(){
 
+    const listName = 'Companies';
     // const columns = ["Title", "Region", "Country"];
-    const items = await this._sp.web.lists.getByTitle("Test Companies").items.select("ID", "Title", "Region", "Country", "Category")();
+
+    const items = await this._sp.web.lists.getByTitle(listName).items.select("ID", fieldNames.name, fieldNames.country, fieldNames.region, fieldNames.category, fieldNames.website).top(2000)();
     this.setState({items: items});
     console.log(this.state.items);
   }
@@ -47,21 +58,17 @@ export default class ListMultilevelGroupView extends React.Component<IListMultil
 
     const groupByFields: IGrouping[] = [
        {
-        name: "Country",
+        name: fieldNames.country,
         order: GroupOrder.descending
       }, {
-        name: "Region",
+        name: fieldNames.region,
         order: GroupOrder.ascending
       },{
-        name: "Category",
+        name: fieldNames.category,
         order: GroupOrder.descending
       }
     ];
 
-     const items2 = this.getCompanies();
-     if(items2.length > 0){
-
-     }
     const items = this.state.items;
     const viewFields = this.getViewFields();
     return (
@@ -87,29 +94,29 @@ export default class ListMultilevelGroupView extends React.Component<IListMultil
       {
         name: 'ID',
         displayName: 'ID',
-        minWidth: 100,
+        minWidth: 50,
+        maxWidth: 100,
+      },
+      {
+        name: fieldNames.name,
+        displayName: 'Name',
+        minWidth: 150,
         maxWidth: 200,
       },
       {
-        name: 'Title',
-        displayName: 'Title',
-        minWidth: 300,
-        maxWidth: 200,
-      },
-      {
-        name: 'Country',
+        name: fieldNames.country,
         displayName: 'Country',
-        minWidth: 200,
+        minWidth: 150,
         maxWidth: 200,
       },
       {
-        name: 'Region',
+        name: fieldNames.region,
         displayName: 'Region',
         minWidth: 200,
         maxWidth: 200,
       },
       {
-        name: 'Category',
+        name: fieldNames.category,
         displayName: 'Category',
         minWidth: 200,
         maxWidth: 200,
@@ -120,138 +127,5 @@ export default class ListMultilevelGroupView extends React.Component<IListMultil
 
   }
 
-  private getCompanies(): any[] {
-
-    const companies = [
-      {
-        name: 'Redwood Studios',
-        Category: 'Studio',
-        Country: 'United States',
-        Region: 'North America'
-      },
-      {
-        name: 'Global Films Distribution',
-        Category: 'Distributor',
-        Country: 'United Kingdom',
-        Region: 'Europe'
-      },
-      {
-        name: 'CineVista Post',
-        Category: 'Post Production',
-        Country: 'France',
-        Region: 'Europe'
-      },
-      {
-        name: 'Oceanic Sales Agency',
-        Category: 'Sales Agent',
-        Country: 'Australia',
-        Region: 'Oceania'
-      },
-      {
-        name: 'Skyline Entertainment',
-        Category: 'Studio',
-        Country: 'Canada',
-        Region: 'North America'
-      },
-      {
-        name: 'Eastern Media Works',
-        Category: 'Post Production',
-        Country: 'India',
-        Region: 'Asia'
-      },
-      {
-        name: 'Nordic Distribution Co.',
-        Category: 'Distributor',
-        Country: 'Ireland',
-        Region: 'Europe'
-      },
-      {
-        name: 'Sunrise Studios',
-        Category: 'Studio',
-        Country: 'Japan',
-        Region: 'Asia'
-      },
-      {
-        name: 'Andean Creative Hub',
-        Category: 'Post Production',
-        Country: 'Argentina',
-        Region: 'South America'
-      },
-      {
-        name: 'African Film Partners',
-        Category: 'Sales Agent',
-        Country: 'South Africa',
-        Region: 'Africa'
-      },
-      {
-        name: 'Global Films Distribution',
-        Category: 'Distributor',
-        Country: 'United Kingdom',
-        Region: 'Europe'
-      },
-      {
-        name: 'Parisian Media Group',
-        Category: 'Studio',
-        Country: 'France',
-        Region: 'Europe'
-      },
-      {
-        name: 'Viking Entertainment',
-        Category: 'Sales Agent',
-        Country: 'Ireland',
-        Region: 'Europe'
-      },
-      {
-        name: 'Alpine Productions',
-        Category: 'Post Production',
-        Country: 'Ireland',
-        Region: 'Europe'
-      },
-      {
-        name: 'Baltic Film Services',
-        Category: 'Distributor',
-        Country: 'Ireland',
-        Region: 'Europe'
-      },
-      {
-        name: 'Iberian Cinematography',
-        Category: 'Studio',
-        Country: 'United Kingdom',
-        Region: 'Europe'
-      },
-      {
-        name: 'Athens Visual Arts',
-        Category: 'Post Production',
-        Country: 'Ireland',
-        Region: 'Europe'
-      },
-      {
-        name: 'Roman Media Works',
-        Category: 'Sales Agent',
-        Country: 'Ireland',
-        Region: 'Europe'
-      },
-      {
-        name: 'Danube Film Partners',
-        Category: 'Distributor',
-        Country: 'United Kingdom',
-        Region: 'Europe'
-      },
-      {
-        name: 'Berlin Creative Studios',
-        Category: 'Studio',
-        Country: 'United Kingdom',
-        Region: 'Europe'
-      },
-      {
-        name: 'Dublin Media Group',
-        Category: 'Post Production',
-        Country: 'Ireland',
-        Region: 'Europe'
-      }
-
-    ];
-
-    return companies;
-  }
+  
 }
